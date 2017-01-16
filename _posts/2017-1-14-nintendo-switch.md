b9c28e8ebe3ea85cda55b41909362e97cd268bfa
Amir R Muntasser <arkmuntasser@gmail.com>
2017-01-15 21:59:57 -0700

    added blog post

---
+++ _posts/2017-1-14-nintendo-switch.md
@@ -0,0 +1,36 @@
+---
+layout: post
+date: 2017-1-14
+title: The Nintendo Switch is Beautiful
+slug: nintendo-switch-beautiful
+excerpt: What are the hex values of that blue and that red?
+tags: nintendo switch
+image: /assets/posts/2017-1-14-nintendo-switch/bundle_color_tabletop.jpg
+images: [/assets/posts/2017-1-14-nintendo-switch/bundle_color_box.jpg, /assets/posts/2017-1-14-nintendo-switch/bundle_color_console.jpg, /assets/posts/2017-1-14-nintendo-switch/bundle_color_portable.jpg, /assets/posts/2017-1-14-nintendo-switch/bundle_color_tabletop.jpg]
+---
+
+<div class="flex-video">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/Ao-3D8jmi90" frameborder="0" allowfullscreen></iframe>
+</div>
+<div class="sep"></div>
+
+Nintendo finally fully reveled their new console, the Nintendo Switch. I have been infatuated with it ever since. I mean, just look at it! That has to be the prettiest looking console every. I thought the Xbox One S was sleek, but this thing is on a whole other level.
+
+I will admit, I am a sucker for the bright colorful controllers. They really do it for me. There's a console that is all black and it' just not as attractive. I can't wait to pick this up when it drops this March.
+
+Now let's just hope the software is at least half as pretty though I know that's a lot to ask with Nintendo's record on UI design.
+
+<div style='position:relative;padding-bottom:57%'><iframe src='https://gfycat.com/ifr/FondNegligibleBunny' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>
+<div class="sep"></div>
+
+But the couple seconds the UI was shown does make me optimistic. Of course this will almost certainly change before release and continue to be even more different post release. Oh well, a girl can dream.
+
+<h6>Sources</h6>
+<ul class="no-bullets">
+  <li>
+    The Verge: <a href="http://www.theverge.com/2017/1/13/14265722/nintendo-switch-hands-on-photos" target="_blank" rel="noopener" title="A closer look at the Nintendo Switch">A closer look at the Nintendo Switch</a>
+  </li>
+  <li>
+    Polygon: <a href="http://www.polygon.com/2017/1/13/14261794/nintendo-switch-ui-user-interface" target="_blank" rel="noopener" title="Here is exactly 3 seconds of the Nintendo Switch UI">Here is exactly 3 seconds of the Nintendo Switch UI</a>
+  </li>
+</ul>
--- _site/sw.js
+++ _site/sw.js
@@ -23,71 +23,15 @@ self.oninstall = event => event.waitUntil(async function () {
 
 self.onactivate = event => event.waitUntil(self.clients.claim());
 
-// Matches paths like `/`, `/index.html`, `/about/` or `/about/index.html`.
-// So when this regexp matches, we know we have to _build_ a response.
-const toplevelSection = /([^/]*)(\/|\/index.html)$/;
 self.onfetch = event => {
   // Parse the request URL so we can separate domain, path and query.
   event.parsedUrl = new URL(event.request.url);
 
   // If this regexp matches, build a response
-  if (event.parsedUrl.pathname.startsWith('/assets/')) {
+  if (event.parsedUrl.pathname.startsWith('/assets/images/')) {
     event.respondWith(caches.match(event.request));
     return;
   }
-  // Otherwise, use our dynamic caching strategy
-  staleWhileRevalidate(event);
+
+  return new Response(null, { status: 404 });
 };
-
-// This function builds a temporary pseudo-event object so we can
-// grab the response as the value of the returned promise.
-function staleWhileRevalidateWrapper(request, waitUntil) {
-  return new Promise(resolve => {
-    staleWhileRevalidate({
-      request,
-      respondWith: resolve,
-      waitUntil
-    })
-  });
-}
-
-// staleWhileRevalidate is a caching strategy. It responds with
-// whatever it got cached (if anything), while updating the cache
-// in the background.
-function staleWhileRevalidate(event) {
-  const fetchedVersion = fetch(event.request);
-  // Since we _might_ be responding with the fetched response
-  // and also using it to populate the cache, we need to make a copy.
-  const fetchedCopy = fetchedVersion.then(response => response.clone());
-  const cachedVersion = caches.match(event.request);
-
-  event.respondWith(async function () {
-    try {
-      // Respond with whatever is ready first, fetched or cached version.
-      // Since fetch() will reject when offline, resolve to cachedVersion
-      // on reject so we always resolve to something.
-      const response = await Promise.race([
-        fetchedVersion.catch(_ => cachedVersion),
-        cachedVersion
-      ]);
-      // However, caches.match() will resolve to `undefined` if there’s
-      // nothing in cache. If that’s the case, wait for the network response.
-      if (!response) {
-        return await fetchedVersion;
-      }
-      return response;
-    } catch(_) {
-      // If nothing returns a valid response (rejects or is undefined),
-      // we just return 404.
-      return new Response(null, {status: 404});
-    }
-  }());
-
-  // event.waitUntil(async function () {
-  //   try {
-  //     const response = await fetchedCopy;
-  //     const cache = await caches.open('dynamic');
-  //     return cache.put(event.request, response);
-  //   } catch(_) {/* eat errors */}
-  // }());
-}
